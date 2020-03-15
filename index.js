3cb37bccc5a5208e3b53b76cca3931d0ca8ed999
Kid <44045911+kidonng@users.noreply.github.com>
2020-03-16 00:50:44 +0800

    publish 1.5.3
    Update URL

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 const open = require('open')
 
-console.info("＼ｱｯｶﾘ～ﾝ／ I'm @kidonng. Nice to meet you!")
-open('https://inu.codes/')
+console.info("＼ｱｯｶﾘ～ﾝ／ Hi, this is Kid. Nice to meet you!")
+open('https://xuann.wang/')
