290434e4f63e6e268796071ead754a45bef77d0b
Kid <hi@xuann.wang>
2024-03-01 19:51:42 +0800

    Improve ICP notice a11y

--- scripts/icp.ts
+++ scripts/icp.ts
@@ -1,3 +1,7 @@
 if (navigator.language === 'zh-CN') {
-	document.getElementById('icp')!.textContent = unescape(atob('JXU4RDYzSUNQJXU1OTA3MjAyMTAxMTAwNSV1NTNGNy0x'))
+	const link = document.createElement('a')
+	link.href = 'https://beian.miit.gov.cn/'
+	link.textContent = unescape(atob('JXU4RDYzSUNQJXU1OTA3MjAyMTAxMTAwNSV1NTNGNy0x'))
+
+	document.getElementById('icp')!.append(link)
 }
