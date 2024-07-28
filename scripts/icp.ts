8b54e95260ce04807864b93b5e51603fbd869f50
Kid <hi@xuann.wang>
2024-07-29 04:02:12 +0800

    Improve ICP template

--- scripts/icp.ts
+++ scripts/icp.ts
@@ -1,7 +1,7 @@
 if (navigator.language === 'zh-CN') {
-	const link = document.createElement('a')
-	link.href = 'https://beian.miit.gov.cn/'
-	link.textContent = unescape(atob('JXU4RDYzSUNQJXU1OTA3MjAyMTAxMTAwNSV1NTNGNy0x'))
+	const template = document.querySelector<HTMLTemplateElement>('#icp')!
 
-	document.getElementById('icp')!.append(link)
+	template.content.querySelector('a')!.textContent = unescape(atob('JXU4RDYzSUNQJXU1OTA3MjAyMTAxMTAwNSV1NTNGNy0x'))
+
+	template.after(template.content)
 }
