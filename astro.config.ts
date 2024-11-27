e2b03ccd2ddeb2a276958633026d52d656923377
Kid <hi@xuann.wang>
2024-11-28 00:09:17 +0800

    Move files to src

--- astro.config.ts
+++ astro.config.ts
@@ -7,5 +7,4 @@ export default defineConfig({
       tailwindcss(),
     ],
   },
-  srcDir: '.',
 })
--- src/scripts/icp.ts
+++ src/scripts/icp.ts
@@ -1,7 +1,5 @@
 if (navigator.language === 'zh-CN') {
   const template = document.querySelector<HTMLTemplateElement>('#icp')!
-
   template.content.querySelector('a')!.textContent = unescape(atob('JXU4RDYzSUNQJXU1OTA3MjAyMTAxMTAwNSV1NTNGNy0x'))
-
   template.after(template.content)
 }
