085f2d1dc3db4a13657a2d1ba387555149876d08
Kid <hi@xuann.wang>
2024-12-23 02:16:29 +0800

    Rename post to channel

--- readme.md
+++ readme.md
@@ -5,6 +5,6 @@ Web Developer
 - [Code](https://github.com/kidonng)
 - [Bluesky](https://bsky.app/profile/xuann.wang)
 - [Write](https://kidonng.notion.site)
-- [Post](https://post.xuann.wang)
+- [Channel](https://channel.xuann.wang)
 - [Chat](https://t.me/kidonng)
 - [Mail](mailto:hi@xuann.wang)
--- src/scripts/channel-link.ts
+++ src/scripts/channel-link.ts
@@ -1,6 +1,6 @@
 const image = new Image()
 image.addEventListener('load', () => {
-  const link = document.querySelector<HTMLAnchorElement>('a[href="https://post.xuann.wang"]')!
+  const link = document.querySelector<HTMLAnchorElement>('a[href="https://channel.xuann.wang"]')!
   link.href = 'https://t.me/s/kichann'
 })
 image.src = 'https://telegram.org/img/favicon-16x16.png'
